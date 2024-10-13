0828ba611fe1f6edffe4f0eb6ae119082bf37cef
Alex <aleksadr.kulish@gmail.com>
2024-10-13 12:40:38 +0300

    Заменил сервис HTTP запросов к сайту СГМУ
    Т.к. undici выдавал ошибку со статусом 301

--- src/json_time_table/json_tt.module.ts
+++ src/json_time_table/json_tt.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { JSONController } from './json_tt.controller';
 import { JsonTimetableService } from './json_tt.service';
 import { HtmlTimeTableService } from './parser.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [JSONController],
   providers: [JsonTimetableService, HtmlTimeTableService],
   exports: [JsonTimetableService],
--- src/json_time_table/parser.service.ts
+++ src/json_time_table/parser.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { request } from 'undici';
 import { parse, HTMLElement } from 'node-html-parser';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 //http://ruz.nsmu.ru/?week=0&group=%D0%9B%D0%941/00000000178&spec=%D0%9B%D0%B5%D1%87%D0%B5%D0%B1%D0%BD%D0%BE%D0%B5%20%D0%B4%D0%B5%D0%BB%D0%BE#metka
 @Injectable()
 export class HtmlTimeTableService {
+  constructor(private readonly httpService: HttpService) {}
+
   private async loadHtmlTimeTable4Week(
     grope: string,
     spec: string,
     week: number,
   ): Promise<HTMLElement> {
-    const { statusCode, headers, trailers, body } = await request(
-      `http://ruz.nsmu.ru/?week=${week}&group=${grope}&spec=${spec}`,
+    const { status, data } = await firstValueFrom(
+      this.httpService.get(
+        `http://ruz.nsmu.ru/?week=${week}&group=${grope}&spec=${spec}`,
+      ),
     );
-    if (statusCode >= 200 && statusCode < 300) {
-      const timetable = parse(await body.text());
+    if (status >= 200 && status < 300) {
+      const timetable = parse(await data);
       return timetable;
     }
-    throw new Error(`status ${statusCode}`);
+    throw new Error(`status ${status}`);
   }
 
   async loadHtmlTimeTable(grope: string, spec: string): Promise<HTMLElement[]> {
